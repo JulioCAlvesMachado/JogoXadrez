f4586121deac0f7500f812eaad0fa80160710398
Julio Machado <juliocarlos00@hotmail.com>
2019-11-25 05:31:01 -0300

    Melhorando a implementação do método ImprimirTabuleiro

--- ProjetoXadrez/Program.cs
+++ ProjetoXadrez/Program.cs
@@ -8,12 +8,25 @@ namespace ProjetoXadrez
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
 
-            Console.WriteLine(pos.ToPosicao());
-            Console.ReadLine();
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.ImprimirTabuleiro(tab);
+
+                Console.ReadLine();
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- ProjetoXadrez/Tela.cs
+++ ProjetoXadrez/Tela.cs
@@ -9,14 +9,36 @@ namespace ProjetoXadrez
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + "|");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
-                    if(tab.Peca(i,j) == null)
+                    if (tab.Peca(i, j) == null)
                         Console.Write("- ");
                     else
-                    Console.Write(tab.Peca(i, j) + " ");
+                    {
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  ---------------- " +
+                "\n  a b c d e f g h");
+        }
+
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(peca);
+                Console.ResetColor();
             }
 
         }
