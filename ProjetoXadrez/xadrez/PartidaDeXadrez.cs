b2e1e2ecce77134baf5b4d193162159e24e88450
Julio Machado <juliocarlos00@hotmail.com>
2019-11-27 03:55:25 -0300

    Jogada especial Promocao

--- ProjetoXadrez/xadrez/PartidaDeXadrez.cs
+++ ProjetoXadrez/xadrez/PartidaDeXadrez.cs
@@ -137,6 +137,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque! ");
             }
 
+
+            Peca p = Tab.Peca(destino);
+
+            //#JogadaEspecial Promocao
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha == 0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    Pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    Pecas.Add(dama);
+                }
+            }
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
                 Xeque = true;
             else
@@ -151,9 +167,7 @@ namespace xadrez
                 Turno++;
                 MudaJogador();
             }
-
-
-            Peca p = Tab.Peca(destino);
+ 
             //#Jogada Especial En Passant
             if (p is Peao && (destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2))
                 VulneravelEnPassant = p;
