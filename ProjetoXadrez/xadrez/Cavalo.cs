d1aae519a36aeec449242a5b533b668b6b30852e
Julio Machado <juliocarlos00@hotmail.com>
2019-11-27 03:15:38 -0300

    Jogada especial roque pequeno e roque grande

--- ProjetoXadrez/xadrez/Cavalo.cs
+++ ProjetoXadrez/xadrez/Cavalo.cs
@@ -1,10 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using tabuleiro;
 
-namespace ProjetoXadrez.xadrez
+namespace xadrez
 {
-    class Cavalo
+    class Cavalo : Peca
     {
+
+        public Cavalo(Tabuleiro Tab, Cor Cor) : base(Tab, Cor)
+        {
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.Peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha - 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna + 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha + 2, Posicao.Coluna - 1);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 2);
+            if (Tab.PosicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
+        public override string ToString()
+        {
+            return "C";
+        }
     }
+    
 }
